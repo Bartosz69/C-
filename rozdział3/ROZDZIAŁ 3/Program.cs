19d1dbba2c5ffdd1ef75b1336b5e37e64b7fae62
Bartosz69 <59618624+Bartosz69@users.noreply.github.com>
2020-11-17 03:11:08 +0100

    Zadania rozdzial 3

---
+++ rozdział3/ROZDZIAŁ 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROZDZIAŁ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double przychod = 1000.00, koszty = 1020.50;
+            double dochod = przychod - koszty;
+            if ( dochod == 0)
+            {
+                Console.WriteLine("Mamy dochód zerowy");
+            }
+            else
+            {
+                if (dochod > 0)
+                    Console.WriteLine("Mamy zysk");
+                else
+                    Console.WriteLine("Mamy stratę");
+            }
+        }
+    }
+}
